469a4ed1d3a4ba4594a3f5f450b9b1ae0e06ab81
Mikhail Filipchuk <mfilipch@e1z3r6p17.42.us.org>
2017-01-31 12:20:59 -0800

    parser of users url and parser of users info

---
+++ get_user_info.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import urllib
+import pandas as pd
+
+if __name__ == '__main__':
+	with open('appcreds.txt', 'r') as credfile:
+		uid, secret = credfile.read().splitlines()
+
+	r = requests.post("https://api.intra.42.fr/oauth/token", data={'grant_type': 'client_credentials', 'client_id': uid, 'client_secret': secret})
+	r.raise_for_status()
+	access_token = json.loads(r.text)['access_token']
+	print(access_token)
+	df = pd.read_csv('users_url.csv')
+	people = []
+	#for i in range(200, 301):
+	for i in range(1, len(df)):
+		print(i)
+		f = urllib.request.urlopen(df['url'][i]+('?access_token=%s' % (access_token)))
+		res = json.loads(f.read())
+		#print(res)
+		people.append(res)
+	
+	#output as json
+	with open('users_info.txt', 'w') as outfile:
+	    json.dump(people, outfile)
+	
+	#output as DataFrame
+	#df = pd.DataFrame(pd.read_json(json.dumps(people)))
+	#df.to_csv('users_info.csv')
+	
+	#print(pd.read_csv('users_info.csv'))
