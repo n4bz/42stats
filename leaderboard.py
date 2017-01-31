e01777e75dae6440bfb76a400c3b803abb436baa
Mikhail Filipchuk <mfilipch@e1z2r9p39.42.us.org>
2017-01-30 23:30:58 -0800

    basic terminal leaderboard finished

--- leaderboard.py
+++ leaderboard.py
@@ -1,23 +1,39 @@
 import plotly.plotly as py
 from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
 import json
 import pandas as pd
+from pivottablejs import pivot_ui
 
 with open('users_info.txt', 'r') as file:
 	js0 = json.loads(file.read())
-level = []
+id_42 = 1
+id_pisc_c = 4
+level_42 = []
+level_pisc_c = []
 for item in js0:
 	try:
-		level.append(round(item['cursus_users'][0]['level'], 2))
+		level_pisc_c.append(*[round(el['level'], 2) for el in item['cursus_users'] if el['cursus_id'] == 4])
+		try:
+			level_42.append(*[round(el['level'], 2) for el in item['cursus_users'] if el['cursus_id'] == 1])
+		except:
+			level_42.append(0)
 	except:
-		level.append(0)
+		level_pisc_c.append(0)
+		level_42.append(0)
 df0 = pd.DataFrame(js0)
-df0.loc[:,'level'] = pd.Series(level, index=df0.index)
-df0 = df0.sort_values(['level', 'last_name'], ascending=[0, 1]).reset_index(drop=True)
+df0.loc[:,'level_42'] = pd.Series(level_42, index=df0.index)
+df0.loc[:,'level_C'] = pd.Series(level_pisc_c, index=df0.index)
+df0['user'] = '<a href="https://profile.intra.42.fr/users/' + df0['login'] + '">' + df0['login'] + '</a>' 
+#df0.loc[:,'cursus'] = pd.Series(cursus, index=df0.index)
+df0 = df0[df0['pool_year'] >= '2016']
+df_42 = df0.sort_values(['level_42', 'last_name'], ascending=[0, 1]).reset_index(drop=True)
+df_C = df0.sort_values(['level_C', 'last_name'], ascending=[0, 1]).reset_index(drop=True)
 #print(list(df0))
 #'<a href="https://profile.intra.42.fr/users/%s">%s</a>' % (login, login)
-df0['user'] = '<a href="https://profile.intra.42.fr/users/' + df0['login'] + '">' + df0['login'] + '</a>' 
-df0['place'] = df0.index + 1
-print(df0[['place', 'login', 'last_name', 'first_name', 'level']])
-#table = FF.create_table(df0[['place', 'user', 'first_name', 'last_name', 'level']])
-#py.iplot(table, filename='simple_table')
+df_42['place'] = df_42.index + 1
+print(df_42[['place', 'login', 'last_name', 'first_name', 'level_42', 'pool_month','pool_year']])
+df_C['place'] = df_C.index + 1
+print(df_C[['place', 'login', 'last_name', 'first_name', 'level_C', 'pool_month','pool_year']])
+#table = FF.create_table(df_42[['place', 'user', 'last_name', 'first_name', 'level_42', 'pool_month','pool_year']])
+#py.iplot(table, filename='42 US Leaderboard')
