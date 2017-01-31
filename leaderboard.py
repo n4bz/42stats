de3a6a95b7c8e26ba5357c4db48e459e3ee5cbaf
Mikhail Filipchuk <mfilipch@e1z3r6p17.42.us.org>
2017-01-31 11:58:05 -0800

    misc changes

--- leaderboard.py
+++ leaderboard.py
@@ -1,9 +1,7 @@
-import plotly.plotly as py
-from plotly.tools import FigureFactory as FF
-from plotly.graph_objs import *
+#import plotly.plotly as py
+#from plotly.tools import FigureFactory as FF
 import json
 import pandas as pd
-from pivottablejs import pivot_ui
 
 with open('users_info.txt', 'r') as file:
 	js0 = json.loads(file.read())
@@ -24,16 +22,23 @@ for item in js0:
 df0 = pd.DataFrame(js0)
 df0.loc[:,'level_42'] = pd.Series(level_42, index=df0.index)
 df0.loc[:,'level_C'] = pd.Series(level_pisc_c, index=df0.index)
-df0['user'] = '<a href="https://profile.intra.42.fr/users/' + df0['login'] + '">' + df0['login'] + '</a>' 
-#df0.loc[:,'cursus'] = pd.Series(cursus, index=df0.index)
+#df0['user'] = '<a href="https://profile.intra.42.fr/users/' + df0['login'] + '">' + df0['login'] + '</a>' 
 df0 = df0[df0['pool_year'] >= '2016']
 df_42 = df0.sort_values(['level_42', 'last_name'], ascending=[0, 1]).reset_index(drop=True)
 df_C = df0.sort_values(['level_C', 'last_name'], ascending=[0, 1]).reset_index(drop=True)
 #print(list(df0))
-#'<a href="https://profile.intra.42.fr/users/%s">%s</a>' % (login, login)
 df_42['place'] = df_42.index + 1
-print(df_42[['place', 'login', 'last_name', 'first_name', 'level_42', 'pool_month','pool_year']])
 df_C['place'] = df_C.index + 1
-print(df_C[['place', 'login', 'last_name', 'first_name', 'level_C', 'pool_month','pool_year']])
+
+#output 42 rank table to csv
+df_42.to_csv('leaderboard_42.csv')
+
+#print 42 rank table to terminal 
+#print(df_42[['place', 'login', 'last_name', 'first_name', 'level_42', 'pool_month','pool_year']])
+
+#print C piscine rank table to terminal
+#print(df_C[['place', 'login', 'last_name', 'first_name', 'level_C', 'pool_month','pool_year']])
+
+#upload to plotly (results are not visually good though)
 #table = FF.create_table(df_42[['place', 'user', 'last_name', 'first_name', 'level_42', 'pool_month','pool_year']])
 #py.iplot(table, filename='42 US Leaderboard')
